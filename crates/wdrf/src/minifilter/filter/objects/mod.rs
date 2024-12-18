76c76d3a212c78c13b03ae37ab87ae7117fc1482
catalincot <catalincot15@gmail.com>
2024-12-19 00:07:41 +0200

    [minifilter] Save work

--- crates/wdrf/src/minifilter/filter/objects/mod.rs
+++ crates/wdrf/src/minifilter/filter/objects/mod.rs
@@ -2,9 +2,11 @@ mod file_name_info;
 mod flt_callback;
 mod flt_io_param;
 mod flt_related;
+mod flt_volume_type;
 
 pub use flt_callback::*;
 pub use flt_io_param::*;
 pub use flt_related::*;
 
 pub use file_name_info::*;
+pub use flt_volume_type::*;
